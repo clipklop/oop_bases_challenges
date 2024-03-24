8a8293a9a9ed328c5d7a4b079c8f364f14436466
Mischa <siliconchik@gmail.com>
2024-03-25 00:37:39 +0300

    +level 6 tasks

--- level_6/a_log_products.py
+++ level_6/a_log_products.py
@@ -11,32 +11,50 @@
 
 
 class Product:
-    def __init__(self, title: str, price: float):
+    def __init__(self, title: str, price: float) -> None:
         self.title = title
         self.price = price
 
-    def get_info(self):
+    def get_info(self) -> str:
         return f'Product {self.title} with price {self.price}'
 
 
-class PremiumProduct(Product):
-    def increase_price(self):
+class PrintLoggerMixin:
+    def log(self, message: str) -> None:
+        print(message)
+
+
+class PremiumProduct(Product, PrintLoggerMixin):
+    def increase_price(self) -> None:
         self.price *= 1.2
+        self.log(f'Price increased to {self.price}')
 
-    def get_info(self):
-        base_info = super().get_info()
-        return f'{base_info} (Premium)'
+    def get_info(self) -> str:
+        message = super().get_info() + ' (Premium)'
+        # return f'{base_info} (Premium)'
+        self.log(message)
+        return message
 
 
-class DiscountedProduct(Product):
-    def decrease_price(self):
+class DiscountedProduct(Product, PrintLoggerMixin):
+    def decrease_price(self) -> None:
         self.price /= 1.2
+        self.log(f'Price decreased to {self.price}')
 
-    def get_info(self):
-        base_info = super().get_info()
-        return f'{base_info} (Discounted)'
+    def get_info(self) -> str:
+        message = super().get_info() + ' (Discounted)'
+        self.log(message)
+        return message
+        # return f'{base_info} (Discounted)'
 
 
 if __name__ == '__main__':
-    pass
+    pr = PremiumProduct('Картошечка', 33)
+    pr.get_info()
+    pr.increase_price()
+    pr.get_info()
 
+    dp = DiscountedProduct('Огуречек', 312)
+    dp.get_info()
+    dp.decrease_price()
+    dp.get_info()
--- level_6/b_developer.py
+++ level_6/b_developer.py
@@ -1,5 +1,4 @@
 """
-
 Задания:
     1. Создайте класс Developer, который будет наследоваться от класса ItDepartmentEmployee и класса SuperAdminMixin.
     2. Переопределите у класса Developer метод __init__ таким образом, чтобы он на вход принимал еще и язык программирования.
@@ -9,35 +8,45 @@
 
 
 class Employee:
-    def __init__(self, name: str, surname: str, age: int, salary: float):
+    def __init__(self, name: str, surname: str, age: int, salary: float) -> None:
         self.name = name
         self.surname = surname
         self.age = age
         self.salary = salary
 
-    def get_info(self):
+    def get_info(self) -> str:
         return f'{self.name} with salary {self.salary}'
 
 
 class ItDepartmentEmployee(Employee):
-    def __init__(self, name: str, surname: str, age: int, salary: float):
+    def __init__(self, name: str, surname: str, age: int, salary: float) -> None:
         super().__init__(name, surname, age, salary)
         self.salary *= 2
 
 
 class AdminMixin:
-    def increase_salary(self, employee: Employee, amount: float):
+    def increase_salary(self, employee: Employee, amount: float) -> None:
         employee.salary += amount
 
 
 class SuperAdminMixin(AdminMixin):
-    def decrease_salary(self, employee: Employee, amount: float):
+    def decrease_salary(self, employee: Employee, amount: float) -> None:
         employee.salary -= amount
 
 
-# код писать тут
+class Developer(ItDepartmentEmployee, SuperAdminMixin):
+    def __init__(self, name: str, surname: str, age: int, salary: float, language: str) -> None:
+        super().__init__(name, surname, age, salary)
+        self.language = language
+    
+    def get_info(self) -> str:
+        return super().get_info() + f' and {self.language}'
 
 
 if __name__ == '__main__':
-    pass  # код писать тут
-
+    dev = Developer('John', 'Doe', 30, 1000, 'Python')
+    print(dev.get_info())
+    dev.increase_salary(dev, 100)
+    print(dev.get_info())
+    dev.decrease_salary(dev, 100)
+    print(dev.get_info())
